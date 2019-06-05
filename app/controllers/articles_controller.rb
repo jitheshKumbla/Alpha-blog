a65cfce95929f3c9b67cb7382bccca912d25daf2
Jithesh K <jithesh.k@robosoftin.com>
2019-06-05 15:10:01 +0530

    Redundancy removed in codes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
 	def index 
-
+		@articles = Article.all
 	end
 
 	def new
@@ -22,6 +22,28 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		if @article.present?
+			@article.destroy
+			flash[:notice] = "Article was successfully deleted"
+			redirect_to articles_path
+		end
+	end
 
 	private
 		def article_params
