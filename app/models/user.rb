72beaed0a8053ae4b024a9b07995cf6cb7e64be1
Jithesh K <jithesh.k@robosoftin.com>
2019-06-06 22:39:19 +0530

    Association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+
+	has_many :articles
+
+	before_save { self.email = email.downcase }
 	validates :username, presence: true, length: {minimum: 3, maximum: 25}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+
+
 end
