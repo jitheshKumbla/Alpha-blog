97aa8b8eca87d7a3167a4a84e16357ac13e2f77e
Jithesh K <jithesh.k@robosoftin.com>
2019-06-06 23:18:23 +0530

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
 
+	has_secure_password
 
 end
